0abfcb1ce52553c6f957185e6043d6177b8380e3
wuzhenhuo <42853310+wuzhenhuo@users.noreply.github.com>
2024-07-24 20:59:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,6 @@ models = {
     "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"},
     "llama-3.1-70b-Versatile": {"name": "llama-3.1-70b-Versatile", "tokens": 4096, "developer": "Meta"},
     "llama3-70b-8192": {"name": "LLaMA3-70b-8192", "tokens": 8192, "developer": "Meta"},
-    #"llama-3.1-405b-Reasoning": {"name": "llama-3.1-405b-Reasoning", "tokens": 8192, "developer": "Meta"},
     "mixtral-8x7b-32768": {"name": "Mixtral-8x7b-Instruct-v0.1", "tokens": 32768, "developer": "Mistral"},
 }
 
